2ad3466590f22362b0a26fe07859437a45dccac8
Fish Paradise <yxzjerryfish@hotmail.com>
2019-11-27 21:34:58 +0800

    学习

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace WpfApp3
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Content = "fff";
+            test.Text = "qq";
+        }
     }
 }
